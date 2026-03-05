9003529121daf39d62f1800c6d08db01c4cbc3cf
Zara M <mzaramurphy@gmail.com>
2026-03-05 17:15:49 +0000

    python CDK infrastructure_stack.py for S3 bucket, cloudfront distribution of personal page

--- infrastructure/infrastructure/infrastructure_stack.py
+++ infrastructure/infrastructure/infrastructure_stack.py
@@ -1,7 +1,11 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_cloudfront as cloudfront,
+    aws_cloudfront_origins as origins,
+    aws_s3_deployment as s3deploy,
+    CfnOutput,
+    RemovalPolicy,
 )
 from constructs import Construct
 
@@ -10,10 +14,38 @@ class InfrastructureStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "InfrastructureQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(
+            # Holds static profolio website file.
+            self, "PortfolioBucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        distribution = cloudfront.Distribution(
+            # CDN serves via HTTP 
+            self, "PortfolioDistribution",
+            default_behavior=cloudfront.BehaviorOptions(
+                origin=origins.S3BucketOrigin.with_origin_access_control(bucket),
+                viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+            ),
+            default_root_object="index.html",
+        )
+
+        s3deploy.BucketDeployment(
+            # Frontend build injected into S3 bucket 
+            self, "DeployPortfolio",
+            sources=[s3deploy.Source.asset("../frontend/build")],
+            destination_bucket=bucket,
+            distribution=distribution,
+            distribution_paths=["/*"],
+        )
+
+        CfnOutput(
+            # provide the clourfront url when the deployment finishes
+            self, "PortfolioURL",
+            value=f"https://{distribution.distribution_domain_name}",
+            description="personal website URL",
+        )
+
+        
